c0e0a453d1f456a9dbd4275cd8314f49c9445901
Naod M Alemu <nalemu13@uw.edu>
2026-02-02 19:39:24 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = "YOUR_MAPBOX_TOKEN";
+mapboxgl.accessToken = "pk.eyJ1IjoibmFsZW11MTMiLCJhIjoiY21sMGJ1OGtuMGF0cDNlcHVydjRmNHNteCJ9.h4Fv_SYyOaBydO7lyixGyA";
 
 const path = window.location.pathname.toLowerCase();
 const isMap1 = path.includes("map1");
